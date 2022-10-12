cf893ffb5933e01b615fc4174990e1bf626aec33
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-12 21:49:33 +0100

    second upload to vercel

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div>
       <p>I love you my sweet little girl!!!</p>
-      <p> now we add quary</p>
+      <div>
+        ja nie wiem co sie z tym dzieje vercel przestał dziać
+      </div>
     </div>
   );
 }
