a7dee831fce778058e48487189c06fe94d99a772
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-12 14:48:35 +0100

    check one more time intergration

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <p>Hello World!!!</p>
+      <p>I love you my sweet little girl!!!</p>
     </div>
   );
 }
