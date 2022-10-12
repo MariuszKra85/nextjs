c9ac95998252c83ad94530ab02d77a2e1929555f
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-12 21:27:42 +0100

    add end point and prepare to make query

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+
 
 export default function Home() {
   return (
     <div>
       <p>I love you my sweet little girl!!!</p>
+      <p> now we add quary</p>
     </div>
   );
 }
