dd8cfb81493eefd0e9932b45ba0717b645e62da4
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-16 10:17:39 +0100

    delete  query

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,6 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-
-const QUERY_USER = gql`
-  query {
-    allUsers {
-      name
-    }
-  }
-`;
-
 export default function Home() {
-  console.log('dziala!!!');
-  const { data, loading, error } = useQuery(QUERY_USER);
   return (
     <div>
-      <p>{console.log(data)}</p>
       <p>I love you my sweet little girl!!!</p>
       <p>it is work? abc</p>
     </div>
