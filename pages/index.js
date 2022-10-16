104f008c7bf303d48408be9bb1bd0e3ed0d798fb
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-16 10:47:33 +0100

    add log to check data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
 const All_USER = gql`
@@ -9,6 +10,9 @@ const All_USER = gql`
 `;
 
 export default function Home() {
+  const { data, error, loading } = useQuery(All_USER);
+  console.log('dziala');
+  console.log('data');
   return (
     <div>
       <p>I love you my sweet little girl!!!</p>
